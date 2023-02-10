a83c1b32f497326483cb99a6739a574e321c27e2
Vincent Lanier <vlanier@uoregon.edu>
2023-02-10 13:22:55 -0800

    added example 1 200km test draft

--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -6,6 +6,42 @@ Write your tests HERE AND ONLY HERE.
 
 import nose    # Testing framework
 import logging
+import acp_times
+import arrow
+
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.WARNING)
 log = logging.getLogger(__name__)
+
+def test_example_one_200km_brevet():
+    """
+    Tests a 200km brevet with controls at
+    60km 120km 175km and 205km finish.
+    This is example one from 
+    https://rusa.org/pages/acp-brevet-control-times-calculator
+    """
+    control_locations = [60, 120, 175, 205]
+    times = acp_times.get_times_from_list(control_locations, 200, '2000-01-01 00:00:00')
+
+    truth_open = {
+        60  : arrow.get('2000-01-01 01:46:00', 'YYYY-MM-DD HH:mm:ss'),
+        120 : arrow.get('2000-01-01 03:32:00', 'YYYY-MM-DD HH:mm:ss'),
+        175 : arrow.get('2000-01-01 05:09:00', 'YYYY-MM-DD HH:mm:ss'),
+        205 : arrow.get('2000-01-01 05:53:00', 'YYYY-MM-DD HH:mm:ss')
+    }
+
+    truth_close = {
+        60  : arrow.get('2000-01-01 04:00:00', 'YYYY-MM-DD HH:mm:ss'),
+        120 : arrow.get('2000-01-01 08:00:00', 'YYYY-MM-DD HH:mm:ss'),
+        175 : arrow.get('2000-01-01 11:40:00', 'YYYY-MM-DD HH:mm:ss'),
+        205 : arrow.get('2000-01-01 13:30:00', 'YYYY-MM-DD HH:mm:ss')
+    }
+
+    for time in times:
+        log.debug(f"{times[time]['open_time']} == {truth_open[time]}")
+        assert(times[time]['open_time'] == truth_open[time])
+        log.debug(f"{times[time]['close_time']} == {truth_close[time]}")
+        assert(times[time]['close_time'] == truth_close[time])
+
+# test for edges of locations, 200, 400, 600
+# solution - 
